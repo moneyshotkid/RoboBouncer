b2e062dc7abd24447dbd31f6a11b22b83aa578c2
Nick Nguyen <its.nicknguyen@gmail.com>
2018-12-09 20:01:13 -0800

    Create makecall.php
    Replace with your twilio SID and Token.  and replace your target phone number in {{Target Phone Number +17145551234}} and  {{Twilio Phone Number +17145551212}}

---
+++ makecall.php
@@ -0,0 +1,26 @@
+
+<?php
+//Load Twilio SDK
+require_once 'vendor/autoload.php';
+use Twilio\Rest\Client;
+
+
+$RecordingUrl=$_REQUEST['RecordingUrl']; 
+$cid=$_REQUEST["cid"];
+
+// Find your Account Sid and Auth Token at twilio.com/console
+$sid = "AC*******************";
+$token = "**********************";
+
+$twilio1 = new Client($sid, $token);
+$twilio2 = new Client($sid, $token);
+
+$call1 = $twilio1->calls($cid)->fetch();
+
+//Replace with phone numbers
+$call2 = $twilio2->calls->create("{{Target Phone Number +17145551234}}", // to
+                        "{{Twilio Phone Number +17145551212}}", // from
+                        array("url" => "playrecording.php?RecordingUrl=".$RecordingUrl."&cid=".$cid)
+               );
+
+?>
